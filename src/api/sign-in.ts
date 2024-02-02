7dd955d19a90cabfdc5873ed1c33c007fc48960a
Wellington Prado <wellington@deskree.com>
2024-02-02 00:30:57 -0300

    fix: Sign-in Endpoint URL is now correct

--- src/api/sign-in.ts
+++ src/api/sign-in.ts
@@ -5,7 +5,7 @@ export interface SignInBody {
 }
 
 export async function signIn({ email }: SignInBody) {
-  await api.post('/https://pizzashop.api-dev.deskree.com/api/v1/io/sign-in', {
+  await api.post('https://pizzashop.api-dev.deskree.com/api/v1/io/sign-in', {
     email,
   })
 }
